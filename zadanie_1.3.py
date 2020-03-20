1dbfb7739b0a9d2cc624df1b066a4a0e743f220b
rafal_mizera <rafalmizera11@gmail.com>
2020-03-20 14:35:30 +0100

    zadanie_1.3

--- zadanie_1.3.py
+++ zadanie_1.3.py
@@ -12,21 +12,11 @@ BMI = float(masa) / (float(wzrost) / 100) **2
 
 print(f"Twoje BMI wynosi: {BMI:.2f}")
 
-if BMI < 16:
+if BMI <= 18.49:
     warunek = ("Masz niedowagę. Powinieneś przytyć")
-if BMI >= 16 and BMI < 17:
-    warunek = "Masz nadwagę. Powinieneś schudnąć"
-if BMI >= 17 and BMI < 18.49:
-    warunek = "Masz niedowagę. Powinieneś przytyć"
 if BMI >= 18.5 and BMI < 24.99:
     warunek = "Twoja waga jest odpowiednia"
-if BMI >= 25 and BMI < 29.99:
-    warunek = "Masz nadwagę. Powinieneś schudnąć"
-if BMI >= 30 and BMI < 34.99:
-    warunek = "Masz nadwagę. Powinieneś schudnąć"
-if BMI >= 35 and BMI < 35:
-    warunek = "Masz nadwagę. Powinieneś schudnąć"
-if BMI >= 35:
+if BMI >= 25:
     warunek = "Masz nadwagę. Powinieneś schudnąć"
 
 print(warunek)
