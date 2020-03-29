3458a9ce74cf5e3c7550774f885b44976b041a47
rafal_mizera <rafalmizera11@gmail.com>
2020-03-29 14:05:08 +0200

    niedziela 29.03

--- zjazd_II_online/zad_13.py
+++ zjazd_II_online/zad_13.py
@@ -2,11 +2,11 @@
 # na podstawie temperatur wprowadzonych przez użytkownika
 
 x = 7
-temperatura = 0
+temperatura = 0 # wartość startowa temperatury
 
 while x > 0:
     t = int(input("Podaj temperaurę: "))
     x -= 1
     temperatura += t  # suma wprowadzonych
 
-print(f"Średnia temperatur to: {temperatura / 7} ")
+print(f"Średnia temperatur to: {temperatura / 7:.2f} ")
