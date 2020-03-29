a450fe3e069c63508b87a1ec52465e34951a8797
rafal_mizera <rafalmizera11@gmail.com>
2020-03-29 15:19:10 +0200

    niedziela 29.03

--- zjazd_II_online/zad_14.py
+++ zjazd_II_online/zad_14.py
@@ -1,15 +1,15 @@
-min_x = None
-max_x = None
+mini_x = None
+maxy_x = None
 
 while True:
     number = input("wprowadź liczbę lub x by zakonczyc: ")
     if number == "x":
         break
     number = int(number)
-    if min_x is None or number < min_x:
-        min_x = number
-    if max_x is None or number > max_x:
-        max_x = number
+    if mini_x is None or number < mini_x:
+        mini_x = number
+    if maxy_x is None or number > maxy_x:
+        maxy_x = number
 
-print(f"Wartość maksymalna: {max_x}")
-print(f"Wartość minimalna: {min_x}")
+print(f"Wartość maksymalna: {maxy_x}")
+print(f"Wartość minimalna: {mini_x}")
