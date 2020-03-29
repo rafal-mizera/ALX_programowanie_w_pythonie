5b4df663080269fc19f97c7273316aaa91bba4f9
rafal_mizera <rafalmizera11@gmail.com>
2020-03-29 10:50:28 +0200

    niedziela29.03

--- zjazd_II_online/zadanie_7.py
+++ zjazd_II_online/zadanie_7.py
@@ -3,4 +3,7 @@
 
 liczba = int(input("Podaj dowolną liczbę całkowitą: "))
 
-print(liczba / 2 == 0 and liczba / 3 == 0 and liczba > 10 or liczba == 7)
+print("""f
+"liczba jest podzilna przez 2, podzielna przez 3 i większa od 10
+lub jest to liczba 7:
+{liczba / 2 == 0 and liczba / 3 == 0 and liczba > 10 or liczba == 7} ")
---
+++ zjazd_II_online/zadanie_8.py
@@ -0,0 +1,13 @@
+#Napisz program, któru na podstawie wprowadzonych wymiarów opakowania, obliczy jego objętość oraz sprawdzi, czy jest
+# ona większa od 1 litra
+
+a = float(input("Podaj wymiar a (w cm): "))
+b = float(input("Podaj wymiar a (w cm): "))
+c = float(input("Podaj wymiar a (w cm): "))
+
+objetosc = a * b * c
+
+if objetosc > 1000:
+    print("Objętość opakowania jest większa niż 1000 cm3")
+else:
+    print("Objętość opakowania nie jest większa niż 1000 cm3")
