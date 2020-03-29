4fe3270bc08dd2aa139082b8d871faee5dfe6c15
rafal_mizera <rafalmizera11@gmail.com>
2020-03-29 10:19:50 +0200

    poprawione

--- zjazd_II_online/zadanie.py
+++ zjazd_II_online/zadanie.py
@@ -2,6 +2,7 @@
 # wieksza od 10 , mniejsza równa 15, podzielna przez 2
 
 liczba = int(input("Podaj liczbę: "))
-print(bool(liczba > 10))
-print(bool(liczba <= 15))
-print(bool(liczba % 2 == 0 ))
+
+print(f"Liczba jest większa od 10: {liczba > 10}")
+print(f"Liczba jest mniejsza lub równa 15: {liczba <= 15}")
+print(f"Liczba jest podzielna przez 2: {liczba % 2 == 0}")
