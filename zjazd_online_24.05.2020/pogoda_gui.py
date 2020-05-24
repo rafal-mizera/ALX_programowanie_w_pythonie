85ea00be80846adfad5ba44a7213335213ed5900
rafal_mizera <rafalmizera11@gmail.com>
2020-05-24 15:20:51 +0200

    poprawione

--- zjazd_online_24.05.2020/pogoda_gui.py
+++ zjazd_online_24.05.2020/pogoda_gui.py
@@ -8,23 +8,24 @@ def weather_Viewer():
         lokacja = lokacja_entry.get()
         l = get_location_id(lokacja)
         w = get_location_weather(l)
-        wynik.configure(report(w,lokacja))
+        wynik.configure(text=report(w,lokacja))
 
     except ValueError:
         messagebox.showerror("Błędne dane!!", "Popraw dane!")
-
 
 
 root = tkinter.Tk()
 
 pogoda_label = tkinter.Label(master=root, text="Podaj lokalizację: ")
 pogoda_label.pack()
+
 lokacja_entry = tkinter.Entry(master=root)
 lokacja_entry.pack()
 
 
 wynik = tkinter.Label(master=root, text="")
 wynik.pack()
+
 submit = tkinter.Button(master=root, text="Pokaż pogodę", command= weather_Viewer)
 submit.pack()
 
